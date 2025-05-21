9cf72e3089373379c54484736ad2dca74807bd11
Deepak.jain <deepak.jain@bytestechnolab.com>
2025-05-21 11:10:27 +0530

    add dev env url

--- app/javascript/components/auth/Home.jsx
+++ app/javascript/components/auth/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
     try {
       const token = localStorage.getItem('authToken');
 
-      const response = await axios.get('http://localhost:3000/blogs', {
+      const response = await axios.get('http://3.216.51.54/blogs', {
         headers: {
           Authorization: `Bearer ${token}`,
         },
